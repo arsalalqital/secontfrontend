0aff1dd82ec6a2ecb8b94a778cbc7cc0963c631a
arsalalqital <arsalalqital@gmail.com>
2024-09-23 00:18:56 +0700

    tugas frontend2

---
+++ basic-component/src/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <div>
+      <h2>Main Content Component</h2>
+      <p>Welcome to the Main Content!</p>
+    </div>
+  );
+}
+
+export default MainContent;
